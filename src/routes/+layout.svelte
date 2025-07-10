1ccc3ad344891298683ed69a15afd359a70179f2
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-07-10 17:59:57 +0800

    prepend anchor link with base url

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import { Button } from '$lib/components/ui/button';
 	import '../app.css';
 
@@ -7,8 +8,8 @@
 
 <div class="min-h-[100vh]">
 	<nav class="flex w-full space-x-2 border-b px-8 py-4">
-		<Button variant="ghost" href="/">Home</Button>
-		<Button variant="ghost" href="/calculator/safe-refine">Safe refine</Button>
+		<Button variant="ghost" href="{base}/">Home</Button>
+		<Button variant="ghost" href="{base}/calculator/safe-refine/">Safe refine</Button>
 	</nav>
 
 	<main class="mx-auto my-4 flex w-[60ch] flex-col">
