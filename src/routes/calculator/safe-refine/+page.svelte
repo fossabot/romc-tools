eee16073e34f3555f0d86fedc9e0bf3fdb7c5ca2
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-07-11 20:57:30 +0800

    add input for equipment price

--- src/routes/calculator/safe-refine/+page.svelte
+++ src/routes/calculator/safe-refine/+page.svelte
@@ -1,21 +1,27 @@
 <script lang="ts">
 	import type { RefineRange } from '$lib/calc/safe-refine';
 	import { calculate } from '$lib/calc/safe-refine';
+	import { Input } from '$lib/components/ui/input';
 	import { Label } from '$lib/components/ui/label';
 	import { Separator } from '$lib/components/ui/separator';
 	import { Slider } from '$lib/components/ui/slider';
 
+	let equipment_price = $state(0);
 	let refine_range: [RefineRange, RefineRange] = $state([0, 1]);
 	const [refine_from, refine_to] = $derived(refine_range);
 
-	const output = $derived.by(() => {
+	const output = $derived.by((): [string, string][] => {
 		const { zeny, copy, material } = calculate(refine_from, refine_to);
+		const copy_in_zeny = copy * equipment_price;
+		const material_in_zeny = material * 25_000;
+		const total = zeny + copy_in_zeny + material_in_zeny;
 
-		return {
-			zeny: `${zeny.toLocaleString()}z`,
-			equipment: `${copy} copies`,
-			material: `${material} oridecon/elunium/mithril`,
-		};
+		return [
+			['Zeny', `${zeny.toLocaleString()}z`],
+			['Equipment', `${copy} copies / ${copy_in_zeny.toLocaleString()}z`],
+			['Material', `${material} items / ${material_in_zeny.toLocaleString()}z`],
+			['Total zeny', `${total.toLocaleString()}z`],
+		];
 	});
 </script>
 
@@ -24,6 +30,11 @@
 <Separator class="my-2" />
 
 <div class="mt-6 mb-8 grid grid-cols-1 gap-4 sm:grid-cols-2">
+	<div class="flex flex-col space-y-2">
+		<Label for="equipment_price">Equipment price</Label>
+		<Input type="number" bind:value={equipment_price} id="equipment_price" min="0" step="1000" />
+	</div>
+
 	<div class="flex flex-col space-y-2 sm:col-span-2">
 		<Label for="refine_range">Refine range</Label>
 
@@ -36,6 +47,11 @@
 
 <Separator class="my-2" />
 
-<p>Zeny: <span class="font-semibold underline">{output.zeny}</span></p>
-<p>Equipment: <span class="font-semibold underline">{output.equipment}</span></p>
-<p>Material: <span class="font-semibold underline">{output.material}</span></p>
+<div class="grid grid-cols-1 gap-2 sm:grid-cols-2">
+	{#each output as [key, value]}
+		<div class="flex flex-col -space-y-1 text-center sm:text-end">
+			<span class="text-sm font-medium">{key}</span>
+			<span class="col-span-3 text-lg font-semibold underline">{value}</span>
+		</div>
+	{/each}
+</div>
